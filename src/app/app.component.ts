e8efd9e883190ddd6ed1f8a9d43d15f8cccd794a
hieu2504 <hieult25042000@gmail.com>
2022-04-21 14:22:13 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component, VERSION } from '@angular/core';
 
+//Declarator
 @Component({
+  //Metadata
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  user = {
+    name: 'Hieu',
+    age: 22,
+  };
 }
